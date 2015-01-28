7313fc2372b19bb8671d363cd8a9c1f157ec9fbc
Jason Mehring <nrgaway@gmail.com>
2015-01-27 19:34:08 -0500

    Make sure immutable files are locked always to prevent qubes programs from over-writing

--- usr/lib/qubes-whonix/init/init.sh
+++ usr/lib/qubes-whonix/init/init.sh
@@ -3,6 +3,16 @@
 
 source /usr/lib/qubes-whonix/utility_functions
 
+
+# Files that will have the immutable bit set
+# since we don't want them modified by other programs
+IMMUTABLE_FILES=(
+    '/etc/resolv.conf'
+    '/etc/hostname'
+    '/etc/hosts'
+)
+
+
 resetGatewayIP() {
     if [ "${QUBES_WHONIX}" == "gateway" ]; then
         echo "10.152.152.10" > /etc/whonix-netvm-gateway
@@ -11,31 +21,24 @@ resetGatewayIP() {
     fi
 }
 
+
 # Install default IP address to use in search and replace.
 # It gets updated with current IP address as it get updated
 if [ ! -e "/etc/whonix-netvm-gateway" ]; then
     resetGatewayIP
 fi
 
-if [ "${QUBES_WHONIX}" != "template" ]; then
-    # Files that will have the immutable bit set
-    # since we don't want them modified by other programs
-    IMMUTABLE_FILES=(
-        '/etc/resolv.conf'
-        '/etc/hostname'
-        '/etc/hosts'
-    )
+# Make sure all .anondist files in list are immutable
+immutableFilesEnable "${IMMUTABLE_FILES}"
+immutableFilesEnable "${IMMUTABLE_FILES}" ".anondist"
 
-    # Make sure all .anondist files in list are immutable
-    immutableFilesEnable "${IMMUTABLE_FILES}"
-    immutableFilesEnable "${IMMUTABLE_FILES}" ".anondist"
+# Make sure we are using a copy of the annondist file and if not
+# copy the annondist file and set it immutable
+copyAnondist "/etc/resolv.conf"
+copyAnondist "/etc/hosts"
+copyAnondist "/etc/hostname"
 
-    # Make sure we are using a copy of the annondist file and if not
-    # copy the annondist file and set it immutable
-    copyAnondist "/etc/resolv.conf"
-    copyAnondist "/etc/hosts"
-    copyAnondist "/etc/hostname"
-
+if [ "${QUBES_WHONIX}" == "gateway" ] || [ "${QUBES_WHONIX}" == "workstation" ]; then
     # Replace IP addresses in known configuration files / scripts to
     # currently discovered one
     /usr/lib/qubes-whonix/init/replace-ips
