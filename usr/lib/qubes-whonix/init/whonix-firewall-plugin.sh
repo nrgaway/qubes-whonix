063da5b1a2bde150f6aef045c3aacaea97ebc4d5
Jason Mehring <nrgaway@gmail.com>
2015-06-06 09:12:27 -0400

    Added copyright

--- usr/lib/qubes-whonix/init/whonix-firewall-plugin.sh
+++ usr/lib/qubes-whonix/init/whonix-firewall-plugin.sh
@@ -1,8 +1,28 @@
 #!/bin/bash -e
 # vim: set ts=4 sw=4 sts=4 et :
 #
-# Additional whonix_firewall rules called by reference to this file from
-# '/etc/whonix_firewall.d/40_qubes'
+# whonix-firewall-plugin.sh - Provides additional 'Whonix_firewall' rules.
+#
+# Included by whonix_firewall configurations located at:
+#   /etc/whonix_firewall.d/40_qubes
+#
+# This file is part of Qubes+Whonix.
+# Copyright (C) 2014 - 2015 Jason Mehring <nrgaway@gmail.com>
+# License: GPL-2+
+# Authors: Jason Mehring
+#
+#   This program is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License
+#   as published by the Free Software Foundation; either version 2
+#   of the License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 source /usr/lib/qubes-whonix/utility_functions.sh
 
